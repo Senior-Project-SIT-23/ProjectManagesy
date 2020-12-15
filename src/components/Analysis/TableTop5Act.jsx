90b4695350e4e0adc273d6514ef628bcbfcbe5f0
IAMIN <intiratangniramai@gmail.com>
2020-12-15 19:33:15 +0700

    top

--- src/components/Analysis/TableTop5Act.jsx
+++ src/components/Analysis/TableTop5Act.jsx
@@ -1,39 +1,41 @@
 import React from "react"
 import { Grid, Paper, Typography } from "@material-ui/core"
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-
+import Table from "@material-ui/core/Table"
+import TableBody from "@material-ui/core/TableBody"
+import TableCell from "@material-ui/core/TableCell"
+import TableContainer from "@material-ui/core/TableContainer"
+import TableHead from "@material-ui/core/TableHead"
+import TableRow from "@material-ui/core/TableRow"
+import _ from "lodash"
 
 export default function TableTop5Act(props) {
   function createData(id, activity) {
-    return { id, activity };
+    return { id, activity }
   }
-  
+
   const rows = [
-    createData(1,'Frozen yoghurt'),
-    createData(2,'Ice cream sandwich'),
-    createData(3,'Eclair'),
-    createData(4,'Cupcake'),
-    createData(5,'Gingerbread'),
-  ];
+    createData(1, "Frozen yoghurt"),
+    createData(2, "Ice cream sandwich"),
+    createData(3, "Eclair"),
+    createData(4, "Cupcake"),
+    createData(5, "Gingerbread"),
+  ]
   return (
     <div>
-     
       <TableContainer component={Paper}>
-        <div className="font-athiti text-center"><b>5 อันดับกิจกรรมที่นักศึกษาเคยเข้าร่วมมากที่สุด</b></div>
-      <Table  aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>อันดับ</TableCell>
-            <TableCell align="left">กิจกรรม</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
+        <div className="font-athiti text-center">
+          <b>5 อันดับกิจกรรมที่นักศึกษาเคยเข้าร่วมมากที่สุด</b>
+        </div>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              
+              <TableCell align="left">กิจกรรม</TableCell>
+              <TableCell>จำนวนผู้เข้าร่วม</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {/* {rows.map((row) => (
             <TableRow key={row.name} >
               <TableCell component="th" scope="row" width="10" padding="12px">
                 {row.id}
@@ -41,11 +43,21 @@ export default function TableTop5Act(props) {
               <TableCell align="left" padding="12px">{row.activity}</TableCell>
              
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    
+          ))} */}
+            {_.map(props.data, (d, i) => (
+              <TableRow >
+                
+                <TableCell align="left" padding="12px" >
+                  {d.activity_name}
+                </TableCell>
+                <TableCell component="th" scope="row" padding="12px">
+                  {d.Total}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   )
 }
